8426633928f35bde840fb84204b40910aa18ac1d
waqasahmed94 <66775969+waqasahmed94@users.noreply.github.com>
2020-08-23 06:08:35 +0000

    intialial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [count, setcount] = useState(0);
+  let [ismorning, setmorning] = useState(true);
+  return (
+
+    <div className={`app ${ismorning ? 'app2' : ''}`}>
+      <h1>Counter Value is:-{count}</h1>
+
+      <button onClick={() => setcount(++count)}>Increment</button>
+
+      <hr />
+        <h1>Assignment # 4 BootCamp 2020</h1>
+      <hr />
+
+      <h1>Good {ismorning ? 'Morning' : 'Night'}</h1>
+
+      <button onClick={() => setmorning(!ismorning)}>Change Status</button>
+    </div>
+
+  );
+}
+
+export default App;
